6c1de559597dff4e0247d48ea68072dc8f686b1e
Admin <Admin@DESKTOP-UEVKOEC>
2022-12-05 20:59:01 +0500

    изменение ирархии префабов

--- Assets/Body/EquipList.cs
+++ Assets/Body/EquipList.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "EquipList", menuName = "ScriptableObjects/EquipList", order = 1)]
 public class EquipList : ScriptableObject
 {
     public string NameList;
